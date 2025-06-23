90527e819a54535c4a14adc2ab071c381c8ba1ba
hanser_no_neko <rikowodo1989@gmail.com>
2025-06-23 15:15:41 +0800

    vue.js init + sql connect test

---
+++ src/test.py
@@ -0,0 +1,26 @@
+import pymysql as sq
+import flask as fk
+
+app = fk.Flask(__name__)
+
+@app.route('/test', methods=['GET'])
+def hello():
+    data = {'aaa' : 'bbb',
+            'ccc' : 'ddd'}
+    return fk.jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=9982, debug=True)
+
+    # db = sq.connect(host="127.0.0.1",
+    #                 user="root",
+    #                 password="wuhantuMJ0209",
+    #                 port=3306)
+    # print(db.get_server_info())
+    # db.select_db('test')
+    # p = db.cursor()
+
+    # p.execute('select * from aaa')
+
+    # p.close()
+    # db.close()
